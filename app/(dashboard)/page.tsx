13d1c7818491000c5e869f2c7f86e64f59426b82
Leonardo Raggio <leonardo.raggio@gmail.com>
2025-03-13 15:25:48 -0300

    add DataGrid and DataCard components for financial summary display; include date range formatting and percentage change calculations

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,22 +1,9 @@
-"use client";
+import { DataGrid } from "@/components/data-grid";
 
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-
-export default function Home() {
-  const { onOpen } = useNewAccount();
-
+export default function DasboardPage() {
   return (
-    <div>
-      <div className="flex flex-col gap-y-4 mx-auto">
-        <p>
-          This is an authenticated route. You can only see this if you are
-          logged in.
-        </p>
-        <div>
-          <Button onClick={onOpen}>Create New Account</Button>
-        </div>
-      </div>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
 }
